13a3ddd05130a23e8c05d2644a4d9ffc2918bf35
Lars Mueller <appgurulars@gmx.de>
2020-05-07 09:34:31 +0200

    Setup

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Entity Library (`moblib`)
+
+Low-level high-performance entity library
+
+## About
+
+No dependencies. Licensed under the MIT License. Written by Lars Mueller aka LMD or appguru(eu).
+
+## Links
+
+* [GitHub](https://github.com/appgurueu/moblib) - sources, issue tracking, contributing
+* [Discord](https://discordapp.com/invite/ysP74by) - discussion, chatting
+* [Minetest Forum](https://forum.minetest.net/viewtopic.php?t=24671) - (more organized) discussion
+* [ContentDB](https://content.minetest.net/packages/LMD/moblib) - releases (cloning from GitHub is recommended)
+
+## API
+
+Mostly self-documenting code. Mod namespace is `moblib`, containing all variables & functions.
---
+++ main.lua
@@ -0,0 +1,108 @@
+local engine_moveresult = minetest.has_feature("object_step_has_moveresult")
+local sensitivity = 0.01
+function register_entity(name, def)
+    local props = def.lua_properties
+    def.lua_properties = nil
+    local on_activate = def.on_activate or function() end
+    local on_step = def.on_step or function() end
+    if props.moveresult then
+        -- localizing variables for performance reasons
+        local mr = props.moveresult
+        local mr_collides = mr.collides
+        local mr_collisions = mr.collisions
+        local mr_axes = mr.axes
+        local mr_old_velocity = mr.old_velocity
+        local mr_acc_dependent = mr.acceleration_dependent
+        if engine_moveresult and not mr_acc_dependent then
+            function def.on_step(self, dtime, moveresult)
+                if moveresult.collides then
+                    if mr_axes then
+                        local axes = {}
+                        for _, collision in ipairs(moveresult.collisions) do
+                            axes[collision.axis] = true
+                        end
+                        moveresult.axes = axes
+                    end
+                    if mr_old_velocity then
+                        -- TODO make sure old_speed is renamed to old_velocity
+                        moveresult.old_velocity = moveresult.collisions[1].old_velocity
+                    end
+                end
+                on_step(self, dtime, moveresult)
+            end
+        else
+            local old_on_activate = on_activate
+            function on_activate(self, staticdata, dtime)
+                old_on_activate(self, staticdata, dtime)
+                self._last_velocity = self.object:get_velocity()
+            end
+            local old_on_step = on_step
+            function on_step(self, dtime)
+                local obj = self.object
+                local expected_vel = vector.add(self._last_velocity, vector.multiply(obj:get_acceleration(), dtime))
+                local velocity = obj:get_velocity()
+                local diff = vector.subtract(expected_vel, velocity)
+                local collides = vector.length(diff) >= sensitivity
+                local moveresult = {collides = collides}
+                if collides then
+                    if mr_collisions then
+                        local collisions = {}
+                        diff = vector.apply(diff, math.abs)
+                        local new_velocity = self._last_velocity
+                        for axis, component_diff in pairs(diff) do
+                            if component_diff > sensitivity then
+                                new_velocity[axis] = velocity[axis]
+                                table.insert(collisions, {
+                                    axis = axis,
+                                    old_velocity = self._last_velocity,
+                                    new_velocity = new_velocity
+                                })
+                            end
+                        end
+                        moveresult.collisions = collisions
+                    end
+                    if mr_axes then
+                        local axes = {}
+                        diff = vector.apply(diff, math.abs)
+                        for axis, component_diff in pairs(diff) do
+                            if component_diff > sensitivity then
+                                axes[axis] = true
+                            end
+                        end
+                        moveresult.axes = axes
+                    end
+                    if mr_old_velocity then
+                        moveresult.old_velocity = self._last_velocity
+                    end
+                    if mr_acc_dependent then
+                        moveresult.acceleration_dependent = vector.length(vector.subtract(self._last_velocity, velocity)) < sensitivity
+                    end
+                end
+                old_on_step(self, dtime, moveresult)
+                self._last_velocity = obj:get_velocity()
+            end
+            function def._set_velocity(self, velocity)
+                self.object:set_velocity(velocity)
+                self._last_velocity = velocity
+            end
+        end
+    end
+    if props.staticdata then
+        local serializer = ({json = minetest.write_json, serialize = minetest.serialize})[props.staticdata]
+        local deserializer = ({json = minetest.parse_json, serialize = minetest.deserialize})[props.staticdata]
+        local old_on_activate = on_activate
+        function on_activate(self, staticdata, dtime)
+            self._ = (staticdata ~= "" and deserializer(staticdata)) or {}
+            old_on_activate(self, staticdata, dtime)
+        end
+        function def.get_staticdata(self)
+            return serializer(self._)
+        end
+    end
+    def.on_activate = on_activate
+    def.on_step = on_step
+    minetest.register_entity(name, def)
+    return function()
+        -- minetest.register_entity(name, def)
+    end
+end
